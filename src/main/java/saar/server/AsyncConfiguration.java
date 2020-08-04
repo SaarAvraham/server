b70b5427b5246d4552e5b23f7978723307a7d317
SaarAvraham <av.saar93@gmail.com>
2020-08-04 18:59:20 +0300

    .

--- src/main/java/saar/server/AsyncConfiguration.java
+++ src/main/java/saar/server/AsyncConfiguration.java
@@ -1,105 +1,107 @@
-package saar.server;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
-import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.scheduling.annotation.AsyncConfigurer;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.context.request.async.CallableProcessingInterceptor;
-import org.springframework.web.context.request.async.TimeoutCallableProcessingInterceptor;
-import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.mvc.method.annotation.CallableMethodReturnValueHandler;
-
-import java.lang.reflect.Method;
-import java.util.concurrent.Callable;
-
-@Configuration
-@EnableAsync
-@EnableScheduling
-public class AsyncConfiguration implements AsyncConfigurer {
-
-    private final Logger log = LoggerFactory.getLogger(AsyncConfiguration.class);
-
-    @Override
-    @Bean (name = "taskExecutor")
-    public AsyncTaskExecutor getAsyncExecutor() {
-        log.debug("Creating Async Task Executor");
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);
-        executor.setMaxPoolSize(10);
-        executor.setQueueCapacity(25);
-        return executor;
-    }
-
-    @Override
-    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
-        return new SimpleAsyncUncaughtExceptionHandler();
-    }
-
-    /** Configure async support for Spring MVC. */
-    @Bean
-    public WebMvcConfigurer webMvcConfigurerConfigurer(AsyncTaskExecutor taskExecutor, CallableProcessingInterceptor callableProcessingInterceptor) {
-        return new WebMvcConfigurer() {
-            @Override
-            public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
-                configurer.setDefaultTimeout(360000).setTaskExecutor(taskExecutor);
-                configurer.registerCallableInterceptors(callableProcessingInterceptor);
-                WebMvcConfigurer.super.configureAsyncSupport(configurer);
-            }
-        };
-    }
-
-    @Bean
-    public CallableProcessingInterceptor callableProcessingInterceptor() {
-        return new TimeoutCallableProcessingInterceptor() {
-            @Override
-            public <T> void beforeConcurrentHandling(NativeWebRequest request, Callable<T> task) throws Exception {
-                log.error("beforeConcurrentHandling!");
-                super.beforeConcurrentHandling(request, task);
-            }
-
-            @Override
-            public <T> void preProcess(NativeWebRequest request, Callable<T> task) throws Exception {
-                log.error("preProcess!");
-                super.preProcess(request, task);
-            }
-
-            @Override
-            public <T> void postProcess(NativeWebRequest request, Callable<T> task, Object concurrentResult) throws Exception {
-                log.error("postProcess!");
-//                super.postProcess(request, task, concurrentResult);
-
-                if(concurrentResult instanceof RuntimeException){
-                    System.out.println("runtimeexception");
-                    Object o = super.handleTimeout(request, task);
-                }
-            }
-
-            @Override
-            public <T> void afterCompletion(NativeWebRequest request, Callable<T> task) throws Exception {
-                log.error("Completed!");
-                super.afterCompletion(request, task);
-            }
-
-            @Override
-            public <T> Object handleTimeout(NativeWebRequest request, Callable<T> task) throws Exception {
-                log.error("timeout!");
-                return super.handleTimeout(request, task);
-            }
-
-            @Override
-            public <T> Object handleError(NativeWebRequest request, Callable<T> task, Throwable t) throws Exception {
-                log.error("timeout!");
-                return super.handleTimeout(request, task);
-            }
-        };
-    }
-}
+//package saar.server;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+//import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.task.AsyncTaskExecutor;
+//import org.springframework.scheduling.annotation.AsyncConfigurer;
+//import org.springframework.scheduling.annotation.EnableAsync;
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+//import org.springframework.web.context.request.NativeWebRequest;
+//import org.springframework.web.context.request.WebRequest;
+//import org.springframework.web.context.request.async.CallableProcessingInterceptor;
+//import org.springframework.web.context.request.async.TimeoutCallableProcessingInterceptor;
+//import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.servlet.mvc.method.annotation.CallableMethodReturnValueHandler;
+//
+//import java.lang.reflect.Method;
+//import java.util.concurrent.Callable;
+//
+//@Configuration
+//@EnableAsync
+//@EnableScheduling
+//public class AsyncConfiguration implements AsyncConfigurer {
+//
+//    private final Logger log = LoggerFactory.getLogger(AsyncConfiguration.class);
+//
+//    @Override
+//    @Bean (name = "taskExecutor")
+//    public AsyncTaskExecutor getAsyncExecutor() {
+//        log.debug("Creating Async Task Executor");
+//        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//        executor.setCorePoolSize(5);
+//        executor.setMaxPoolSize(10);
+//        executor.setQueueCapacity(25);
+//        return executor;
+//    }
+//
+//    @Override
+//    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+//        return new SimpleAsyncUncaughtExceptionHandler();
+//    }
+//
+//    /** Configure async support for Spring MVC. */
+//    @Bean
+//    public WebMvcConfigurer webMvcConfigurerConfigurer(AsyncTaskExecutor taskExecutor, CallableProcessingInterceptor callableProcessingInterceptor) {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+//                configurer.setDefaultTimeout(360000).setTaskExecutor(taskExecutor);
+//                configurer.registerCallableInterceptors(callableProcessingInterceptor);
+//                WebMvcConfigurer.super.configureAsyncSupport(configurer);
+//            }
+//        };
+//    }
+//
+//    @Bean
+//    public CallableProcessingInterceptor callableProcessingInterceptor() {
+//        return new TimeoutCallableProcessingInterceptor() {
+//            @Override
+//            public <T> void beforeConcurrentHandling(NativeWebRequest request, Callable<T> task) throws Exception {
+//                log.error("beforeConcurrentHandling!");
+//                super.beforeConcurrentHandling(request, task);
+//            }
+//
+//            @Override
+//            public <T> void preProcess(NativeWebRequest request, Callable<T> task) throws Exception {
+//                log.error("preProcess!");
+//                super.preProcess(request, task);
+//            }
+//
+//            @Override
+//            public <T> void postProcess(NativeWebRequest request, Callable<T> task, Object concurrentResult) throws Exception {
+//                log.error("postProcess!");
+////                super.postProcess(request, task, concurrentResult);
+//
+//                if(concurrentResult instanceof RuntimeException){
+//                    System.out.println("runtimeexception");
+//                    Object o = super.handleTimeout(request, task);
+//                }
+//            }
+//
+//            @Override
+//            public <T> void afterCompletion(NativeWebRequest request, Callable<T> task) throws Exception {
+//                log.error("Completed!");
+//                super.afterCompletion(request, task);
+//            }
+//
+//            @Override
+//            public <T> Object handleTimeout(NativeWebRequest request, Callable<T> task) throws Exception {
+//                log.error("timeout!");
+//                return super.handleTimeout(request, task);
+//            }
+//
+//            @Override
+//            public <T> Object handleError(NativeWebRequest request, Callable<T> task, Throwable t) throws Exception {
+//                log.error("timeout!");
+//                WebRequest r;
+//                return super.handleTimeout(request, task);
+//            }
+//        };
+//    }
+//}
--- src/main/java/saar/server/Controller.java
+++ src/main/java/saar/server/Controller.java
@@ -1,29 +1,26 @@
 package saar.server;
 
-import org.apache.catalina.connector.ResponseFacade;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.WriteListener;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Supplier;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -33,6 +30,12 @@ public class Controller {
     private final Logger logger = LoggerFactory.getLogger(Controller.class);
 
     private static int counter = 1;
+
+    private static Map<String, String> get() {
+        HashMap<String, String> stringStringHashMap = new HashMap<>();
+        stringStringHashMap.put("Test", "Test123412421412421");
+        return stringStringHashMap;
+    }
 
     @GetMapping("/counter")
     public @ResponseBody
@@ -46,25 +49,22 @@ public class Controller {
         response.setHeader(
                 "Content-Disposition",
                 "attachment;filename=sample.txt");
+        response.setTrailerFields(Controller::get);
         StreamingResponseBody stream = out -> {
-            for (int i = 0; i < 5000; i++) {
+            for (int i = 0; i < 5; i++) {
                 String msg = i + " dsaflkasjlkadsjgijiotewjaiojteroiajgfmadfagnfkjnwaejnjtkjnwebvngngnfgfhhgftjanbgfdfbgdbhdfgfjknawejtnwaejndsgsdagfsdds "
                         + System.lineSeparator();
 
-                if(i % 10 == 0){
+                if (i % 1 == 0) {
                     out.flush();
                 }
 
                 if (i == 2000) {
 //                    response.sendError(503, "Error while streaming");
 //                    response.reset()
-                    boolean connection = response.containsHeader("Connection");
-                    response.setHeader("connection", "close");
-                    String connection1 = response.getHeader("connection");
+
 
 //                    Thread.currentThread().interrupt();
-                    response.getWriter().close();
-                    response.getOutputStream().close();
 //                    throw new org.springframework.web.context.request.async.AsyncRequestTimeoutException();
 
                 }
@@ -92,12 +92,11 @@ public class Controller {
                             + System.lineSeparator();
                     zipOut.write(msg.getBytes());
 
-
-                    if(i%10 == 0){
+                    if (i % 10 == 0) {
                         zipOut.flush();
                     }
 
-                    if(i == 300){
+                    if (i == 300) {
                         throw new RuntimeException();
                     }
                 }
@@ -109,5 +108,80 @@ public class Controller {
         };
         logger.info("steaming response {} ", stream);
         return new ResponseEntity(stream, HttpStatus.OK);
+    }
+
+
+    private ExecutorService nonBlockingService = Executors
+            .newCachedThreadPool();
+
+    @GetMapping(value = "/sse", produces = "application/json")
+    public SseEmitter handleSse() {
+        SseEmitter emitter = new SseEmitter();
+        nonBlockingService.execute(() -> {
+            try {
+                for (int i = 0; i < 5000; i++) {
+                    String msg = i + " dsaflkasjlkadsjgijiotewjaiojteroiajgfmadfagnfkjnwaejnjtkjnwebvngngnfgfhhgftjanbgfdfbgdbhdfgfjknawejtnwaejndsgsdagfsdds "
+                            + System.lineSeparator();
+                    emitter.send(msg, MediaType.parseMediaType("text/csv"));
+
+                    if (i == 6) {
+                        throw new Exception();
+                    }
+                }
+                // we could send more events
+                emitter.complete();
+            } catch (Exception ex) {
+                emitter.completeWithError(ex);
+
+            }
+        });
+        return emitter;
+    }
+
+    @GetMapping("/servlet")
+    public void hs(final HttpServletResponse response, HttpServletRequest request) throws IOException, ServletException {
+        response.setHeader(
+                "Content-Disposition",
+                "attachment;filename=sample.txt");
+        response.setStatus(200);
+
+        ServletOutputStream out = response.getOutputStream();
+        for (int i = 0; i < 5000; i++) {
+            String msg = i + " dsaflkasjlkadsjgijiotewjaiojteroiajgfmadfagnfkjnwaejnjtkjnwebvngngnfgfhhgftjanbgfdfbgdbhdfgfjknawejtnwaejndsgsdagfsdds "
+                    + System.lineSeparator();
+
+            if (i % 10 == 0) {
+                out.flush();
+            }
+
+            if (i == 2000) {
+//                    response.sendError(503, "Error while streaming");
+//                    response.reset()
+                boolean connection = response.containsHeader("Connection");
+                response.setHeader("connection", "close");
+                String connection1 = response.getHeader("connection");
+
+//                    Thread.currentThread().interrupt();
+                response.getWriter().close();
+                response.getOutputStream().close();
+                ServletOutputStream outputStream = response.getOutputStream();
+                outputStream.setWriteListener(new WriteListener() {
+                    @Override
+                    public void onWritePossible() throws IOException {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        System.out.println();
+                    }
+                });
+                response.sendError(500, "dfsfsd");
+//                    throw new org.springframework.web.context.request.async.AsyncRequestTimeoutException();
+
+            }
+            out.write(msg.getBytes());
+        }
+        ;
     }
 }
